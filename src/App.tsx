b0f456c93e3b94f51366b4891451c228da0761ea
Kasperoid <ivanu9053@gmail.com>
2024-09-01 12:48:13 +0300

    Настройка корневого компонента и контейнера

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import { Layout } from 'antd';
+import React, { useState } from 'react';
+import { ipAddressType } from './types/types';
+import HeaderTracker from './components/HeaderTracker';
+import InfoCard from './components/InfoCard';
+import MapIpContainer from './components/MapIpContainer';
 
 function App() {
-  return <div className="App"></div>;
+  const localIpObj: ipAddressType = {
+    ip: '8.8.8.8',
+    location: {
+      country: 'US',
+      region: 'California',
+      city: 'Mountain View',
+      lat: 37.40599,
+      lng: -122.078514,
+      timezone: '-07:00',
+      geonameId: 5375481,
+      postalCode: '12312',
+    },
+    domains: [
+      '0d2.net',
+      '003725.com',
+      '0f6.b0094c.cn',
+      '007515.com',
+      '0guhi.jocose.cn',
+    ],
+    isp: 'Google LLC',
+  };
+
+  const [ipInfo, setIpInfo] = useState<ipAddressType>(localIpObj);
+  const { Content } = Layout;
+
+  return (
+    <Layout style={{ height: '100vh' }}>
+      <HeaderTracker />
+      <Content style={{ padding: '0 25px', position: 'relative' }}>
+        <InfoCard
+          ip={ipInfo.ip}
+          region={ipInfo.location.region}
+          city={ipInfo.location.city}
+          timezone={ipInfo.location.timezone}
+          isp={ipInfo.isp}
+          postalCode={ipInfo.location.postalCode}
+        />
+        <MapIpContainer />
+      </Content>
+    </Layout>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'normalize.css';
+import './styles/header/header.css';
+import './styles/main/main.css';
+import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: '"Rubik", sans-serif',
+        },
+        components: {
+          Button: {
+            colorPrimary: '#000',
+            algorithm: true,
+          },
+          Input: {
+            colorPrimary: 'hsl(0, 0%, 59%)',
+            algorithm: true,
+          },
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
