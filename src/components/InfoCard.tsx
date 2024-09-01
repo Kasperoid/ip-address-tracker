337d2aad23ee4773153edcfc8293004b2fee1354
Kasperoid <ivanu9053@gmail.com>
2024-09-01 12:49:04 +0300

    Создание информационной компоненты

---
+++ src/components/InfoCard.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Flex, Card, Typography, Space, Divider } from 'antd';
+import { ipAddressType, ipLocationType } from '../types/types';
+
+interface InfoCardProps
+  extends Omit<ipAddressType, 'domain' | 'location'>,
+    Pick<ipLocationType, 'city' | 'region' | 'timezone' | 'postalCode'> {}
+
+type optionsCardType = {
+  name: string;
+  value: string;
+};
+
+const InfoCard = ({
+  ip,
+  region,
+  city,
+  timezone,
+  isp,
+  postalCode,
+}: InfoCardProps) => {
+  const { Paragraph } = Typography;
+
+  const options: optionsCardType[] = [
+    {
+      name: 'IP ADDRESS',
+      value: ip,
+    },
+    {
+      name: 'LOCATION',
+      value: `${region}, ${city} ${postalCode}`,
+    },
+    {
+      name: 'TIMEZONE',
+      value: `UTC ${timezone}`,
+    },
+    {
+      name: 'ISP',
+      value: isp,
+    },
+  ];
+  
+  return (
+    <Flex
+      justify="center"
+      style={{ position: 'absolute', top: -60, left: 0, right: 0 }}
+    >
+      <Card
+        style={{
+          flex: 1,
+          maxWidth: '750px',
+          borderRadius: '14px',
+        }}
+      >
+        <Flex justify="space-between">
+          {options.map((option, index) => (
+            <>
+              <Space direction="vertical" size={0}>
+                <Paragraph
+                  style={{
+                    fontSize: '10px',
+                    fontWeight: '700',
+                    letterSpacing: '1px',
+                    color: 'hsl(0, 0%, 59%)',
+                    marginBottom: 0,
+                  }}
+                >
+                  {option.name}
+                </Paragraph>
+                <Paragraph
+                  style={{
+                    color: 'hsl(0, 0%, 17%)',
+                    fontSize: '18px',
+                    fontWeight: 500,
+                  }}
+                >
+                  {option.value}
+                </Paragraph>
+              </Space>
+              {index + 1 < options.length && (
+                <Divider type="vertical" style={{ height: 'auto' }} />
+              )}
+            </>
+          ))}
+        </Flex>
+      </Card>
+    </Flex>
+  );
+};
+
+export default InfoCard;
