158181ed271789ac184377eb7cfb2462e26b7f69
Kasperoid <ivanu9053@gmail.com>
2024-09-25 17:07:20 +0300

    Создание стилизованного компонента для хранения всего контента

---
+++ src/styles/app/MainContainerStyled.ts
@@ -0,0 +1,8 @@
+import { Layout, LayoutProps } from 'antd';
+import styled from 'styled-components';
+
+export const MainContainerStyled = styled(Layout)<LayoutProps>`
+  && {
+    height: 100vh;
+  }
+`;
