2e321e5c1fd150e71af9a5cabb19a6c1e8cd47f5
Kasperoid <ivanu9053@gmail.com>
2024-09-25 17:07:48 +0300

    Создание стилизованного компонента для хранения внутреннего контента всей страницы

---
+++ src/styles/app/ContainerContentStyled.ts
@@ -0,0 +1,11 @@
+import { Layout } from 'antd';
+import styled from 'styled-components';
+
+const { Content } = Layout;
+
+export const ContainerContentStyled = styled(Content)`
+  && {
+    padding: 0 25px;
+    position: relative;
+  }
+`;
