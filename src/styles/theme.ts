a51c24fba65cb0939006530ed5a49cf659e7a302
Kasperoid <ivanu9053@gmail.com>
2024-09-25 17:01:38 +0300

    Создание базового стиля

---
+++ src/styles/theme.ts
@@ -0,0 +1,25 @@
+export const baseTheme = {
+  color: {
+    primary: '#000',
+    secondary: 'hsl(0, 0%, 59%)',
+    tertiary: 'white',
+  },
+  font: {
+    text: {
+      cardOption: {
+        max: 26,
+        min: 12,
+      },
+    },
+    title: {
+      header: {
+        max: 32,
+        min: 18,
+      },
+      cardOption: {
+        max: 10,
+        min: 8,
+      },
+    },
+  },
+};
