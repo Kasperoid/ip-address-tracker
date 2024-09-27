3d63da95f5f3264c44026cf6c1d97dc8abb97415
Kasperoid <ivanu9053@gmail.com>
2024-09-27 23:20:15 +0300

    Убран тестовый объект

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,31 +1,7 @@
-import { CustomError, ipAddressType } from '../types/types';
-
-export const localIpObj: ipAddressType = {
-  ip: '8.8.8.8',
-  location: {
-    country: 'US',
-    region: 'California',
-    city: 'Mountain View',
-    lat: 37.40599,
-    lng: -122.078514,
-    timezone: '-07:00',
-    geonameId: 5375481,
-    postalCode: '12312',
-  },
-  domains: [
-    '0d2.net',
-    '003725.com',
-    '0f6.b0094c.cn',
-    '007515.com',
-    '0guhi.jocose.cn',
-  ],
-  isp: 'Google LLC',
-};
+import { CustomError } from '../types/types';
 
 export const URL_SEARCH_IP: string =
-  'https://geo.ipify.org/api/v2/country,city';
-
-export const API_KEY: string = 'at_k3xMYfAXP0Nqbo84wvtX80BKrXuTv';
+  'http://ipwho.is/';
 
 export const UNKNOWN_ERROR: CustomError = {
   name: 'Unknown error',
