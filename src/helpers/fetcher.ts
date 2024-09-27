468a9ac8fcdc288c4b3efc8db391ce27fe51283c
Kasperoid <ivanu9053@gmail.com>
2024-09-27 11:29:51 +0300

    Изменение функции отправки запроса к api

--- src/helpers/fetcher.ts
+++ src/helpers/fetcher.ts
@@ -1,38 +1,28 @@
 import axios from 'axios';
-import { API_KEY, UNKNOWN_ERROR, URL_SEARCH_IP } from '../constants/constants';
+import { API_KEY, UNKNOWN_ERROR } from '../constants/constants';
 import { createError } from './createError';
-import { ipAddressType, ipErrorType } from '../types/types';
+import { ipAddressType } from '../types/types';
 
-export const fetcher = async (searchIp: string): Promise<ipAddressType> => {
-  try {
-    const resp = await axios(URL_SEARCH_IP, {
-      params: {
-        apiKey: API_KEY,
-        ipAddress: searchIp,
-      },
-    });
-    return resp.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      if (error.response?.data) {
-        const resultError = createError(
-          error.response.data.messages,
-          error.response.data.code
-        );
-        throw resultError;
-      } else {
-        const resultError = createError(
-          UNKNOWN_ERROR.messages,
-          UNKNOWN_ERROR.code
-        );
-        throw resultError;
-      }
-    } else {
-      const resultError = createError(
-        UNKNOWN_ERROR.messages,
-        UNKNOWN_ERROR.code
-      );
-      throw resultError;
-    }
+export const fetcher = async (url: string, inputStr: string): Promise<ipAddressType | undefined> => {
+  const res = await fetch(`${url}?apiKey=${API_KEY}&ipAddress=${inputStr}`);
+  if (!res.ok) {
+    throw new Error('Ошибка запроса');
   }
-};
+  return await res.json();
+  // try {
+  //   const resp = await axios(url, {
+  //     params: {
+  //       apiKey: API_KEY,
+  //       ipAddress: inputStr,
+  //     },
+  //   });
+  //   return resp.data;
+  // } catch (error) {
+  //   if (axios.isAxiosError(error)) {
+  //     if (error.response) {
+  //       throw new Error(error.response.data)
+  //     }
+  //     throw new Error(error.message)
+  //   }
+  // };
+}
