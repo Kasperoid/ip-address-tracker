7b2fbd58dd6c41d2695c426668c8e586d68c1a1f
Kasperoid <ivanu9053@gmail.com>
2024-09-27 23:21:01 +0300

    Добавление сообщения об ошибке, изменено получение данных с использованием хука useSWRMutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { CustomError, ipAddressType } from './types/types';
 import HeaderTracker from './components/HeaderSearchSection/HeaderTracker';
 import InfoCard from './components/ContentSection/InfoCard';
 import MapIpContainer from './components/MapSection/MapIpContainer';
@@ -7,49 +6,43 @@ import { URL_SEARCH_IP } from './constants/constants';
 import { MainContainerStyled } from './styles/app/MainContainerStyled';
 import { ContainerContentStyled } from './styles/app/ContainerContentStyled';
 import { fetcher } from './helpers/fetcher';
-import useSWR from 'swr';
-import { isIpData } from './helpers/typeGuard';
-import { Button, Result } from 'antd';
+import useSWRMutation from 'swr/mutation';
 import { useEffect } from 'react';
+import { message } from 'antd';
+import { errorApiType } from './types/types';
 
 function App() {
-  const { data, error, isLoading } = useSWR<
-    ipAddressType | undefined,
-    CustomError
-  >(URL_SEARCH_IP, () => fetcher(URL_SEARCH_IP, ''), {
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
+  const [messageApi, contextHolder] = message.useMessage();
+  const errorAlert = (message: string) => {
+    messageApi.open({
+      type: 'error',
+      content: message,
+    });
+  };
+
+  const { data, isMutating, trigger } = useSWRMutation(URL_SEARCH_IP, fetcher, {
+    throwOnError: false,
+    onError: error => {
+      errorAlert((error as errorApiType).message);
+    },
   });
-  const position: LatLngTuple | undefined = isIpData(data)
-    ? [data.location.lat, data.location.lng]
-    : undefined;
+  const position: LatLngTuple | undefined =
+    data?.latitude && data?.longitude
+      ? [data.latitude, data.longitude]
+      : undefined;
 
   useEffect(() => {
-    console.log(error);
-  }, [error]);
+    trigger('');
+  }, [trigger]);
 
   return (
     <MainContainerStyled>
-      <HeaderTracker loading={isLoading} />
-      {error ? (
-        <Result
-          status="error"
-          title="Submission Failed"
-          subTitle="Please check and modify the following information before resubmitting."
-          extra={[
-            <Button type="primary" key="console">
-              Go Console
-            </Button>,
-            <Button key="buy">Buy Again</Button>,
-          ]}
-        />
-      ) : (
-        <ContainerContentStyled>
-          <InfoCard {...data} {...data?.location} loading={isLoading} />
-          <MapIpContainer position={position} isp={data?.isp} />
-        </ContainerContentStyled>
-      )}
+      {contextHolder}
+      <HeaderTracker trigger={trigger} />
+      <ContainerContentStyled>
+        <InfoCard {...data} loading={isMutating} />
+        <MapIpContainer position={position} isp={data?.connection?.isp} />
+      </ContainerContentStyled>
     </MainContainerStyled>
   );
 }
