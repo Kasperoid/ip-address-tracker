0be103a2d555d51ee810271b835926b256ecb898
Kasperoid <ivanu9053@gmail.com>
2024-09-27 23:19:58 +0300

    Убран try catch, проверка осуществляется по другому с использованием if else, исходя из полученных данных с сервера

--- src/helpers/fetcher.ts
+++ src/helpers/fetcher.ts
@@ -1,28 +1,19 @@
 import axios from 'axios';
-import { API_KEY, UNKNOWN_ERROR } from '../constants/constants';
-import { createError } from './createError';
 import { ipAddressType } from '../types/types';
+import { isErrorApi } from './typeGuard';
+import { createError } from './createError';
+import { UNKNOWN_ERROR } from '../constants/constants';
 
-export const fetcher = async (url: string, inputStr: string): Promise<ipAddressType | undefined> => {
-  const res = await fetch(`${url}?apiKey=${API_KEY}&ipAddress=${inputStr}`);
-  if (!res.ok) {
-    throw new Error('Ошибка запроса');
+export const fetcher = async (url: string, { arg }: { arg: string }): Promise<ipAddressType | undefined> => {
+  const resp = await axios(`${url}/${arg}`);
+  if (!resp.data.success) {
+    let error;
+    if (isErrorApi(resp.data)) {
+      error = createError(resp.data.message, 402, 'Error')
+    } else {
+      error = createError(UNKNOWN_ERROR.message, UNKNOWN_ERROR.code, UNKNOWN_ERROR.name)
+    }
+    throw error
   }
-  return await res.json();
-  // try {
-  //   const resp = await axios(url, {
-  //     params: {
-  //       apiKey: API_KEY,
-  //       ipAddress: inputStr,
-  //     },
-  //   });
-  //   return resp.data;
-  // } catch (error) {
-  //   if (axios.isAxiosError(error)) {
-  //     if (error.response) {
-  //       throw new Error(error.response.data)
-  //     }
-  //     throw new Error(error.message)
-  //   }
-  // };
+  return resp.data
 }
